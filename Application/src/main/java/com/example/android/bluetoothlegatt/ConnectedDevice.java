ce6d11fd816e07bb67ff035183dd962af270948a
itsFrank <frankobrien11@yahoo.ca>
2016-04-02 16:58:11 -0400

    initial commit - basic launcher page

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/ConnectedDevice.java
@@ -0,0 +1,17 @@
+package com.example.android.bluetoothlegatt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Francis on 4/2/2016.
+ */
+public class ConnectedDevice extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connected_device);
+    }
+}
